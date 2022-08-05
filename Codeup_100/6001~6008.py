338778911816facef075260c2b0107cfc73cfd02
lucky.doyun <doyun.jang.0128@gmail.com>
2022-08-05 14:23:08 +0900

    codeup 100제 풀이

---
+++ Codeup_100/6001~6008.py
@@ -0,0 +1,25 @@
+# codeup _ 100제 
+# 기초-출력
+
+print('Hello')
+
+print('Hello World')
+
+print('Hello')
+print('World')
+
+print("'Hello'")
+
+print('"Hello World"')
+
+# "!@#$%^&*()'
+
+print('"!@#$%^&*()\'')
+
+
+# "C:\Download\'hello'.py"
+
+print('"C:\Download\\\'hello\'.py"')
+
+# print("Hello\nWorld")
+print('print("Hello\\nWorld")')
