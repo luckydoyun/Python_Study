91f7b2282c58041a45750f2b685dc79834cff72f
lucky.doyun <doyun.jang.0128@gmail.com>
2022-08-09 23:46:55 +0900

    2022.08.09 Python study

---
+++ Codeup_100/6025~6031.py
@@ -0,0 +1,31 @@
+# codeup _ 100제 
+# 기초-값변환 & 출력변환
+
+# a, b = map(int, input().split())
+# print(a + b)
+
+# a = float(input())
+# b = float(input())
+
+# print(a + b)
+
+# 16진수 소문자
+# d = int(input())
+# print('%x' %(d))
+
+# 16진수 대문자
+# d = int(input())
+# print('%X' %(d))
+
+# 16진수 입력받고 8진수로 변경
+# d = input()
+# n = int(d, 16)
+# print('%o' %(n))
+
+# 영문자를 입력받고 10진수 유니코드 값으로 출력
+# n = ord(input())
+# print(n)
+
+# 정수(32 ~ 126 범위)를 입력받아 유니코드 문자로 변환
+c = int(input())
+print(chr(c))
