30014e0d290a7fbf099473d0001ead45d9b292b9
lucky.doyun <doyun.jang.0128@gmail.com>
2022-08-09 23:08:54 +0900

    2022.08.09 Python Study.

---
+++ Codeup_100/6009~6024.py
@@ -0,0 +1,68 @@
+# codeup _ 100제 
+# 기초-입출력
+
+# print(input())
+
+# print(int(input()))
+
+# print(float(input()))
+
+# a = int(input())
+# b = int(input())
+# print(a)
+# print(b)
+
+# a = input()
+# b = input()
+# print(b)
+# print(a)
+
+# f = float(input())
+# print(f)
+# print(f)
+# print(f)
+
+# a, b = map(int, input().split())
+# print(a)
+# print(b)
+
+# c, d = input().split()
+# print(d, c)
+
+# s = input()
+# print(s, s, s)
+
+# The print() that Python Function can use to varied options.
+# sep fullname is 'separation'
+# a, b = input().split(':')
+# print(a, b, sep=':')
+
+# The format options is useful.
+# 숫자를 사용한 format 
+# ex)  print( "{0}월 {1}일 입니다." .format(10,31))
+# %기호를 사용한 format
+# ex) print("%s을 %d개 주세요." %("아이스크림", 10))
+
+# y, m, d = input().split('.')
+# print(d,m,y, sep='-')
+
+# f, e = input().split('-')
+# print(f+e) #print(f,e, sep='')
+
+# s = input()
+
+# for x in range(len(s)):
+#     print(s[x])
+
+# s = input()
+
+# print(s[0:2], end=' ')
+# print(s[2:4], end=' ')
+# print(s[4:6])
+# print(s[0:2], s[2:4], s[4:6], sep=' ')
+
+# h, m, s = input().split(':')
+# print(m)
+
+a, b = input().split()
+print(a+b)
