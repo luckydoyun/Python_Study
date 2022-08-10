fb9e4b23b01892b493674cc28a545fc648c04d47
lucky.doyun <doyun.jang.0128@gmail.com>
2022-08-10 23:32:19 +0900

    2022.08.10 Python study

---
+++ Codeup_100/6032~6047.py
@@ -0,0 +1,57 @@
+# codeup _ 100제 
+# 기초-산술연산
+
+
+# n = int(input())
+# print(-n)
+
+# c = ord(input())
+# print(chr(c+1))
+
+# a, b = map(int, input().split())
+# print( a - b )
+
+# a, b = map(float, input().split())
+# print(a * b)
+
+# w, n = input().split()
+# print(w * int(n))
+
+# n = int(input())
+# s = input()
+# print(n * s)
+
+# a, b = map(float, input().split())
+# print(a**b)
+
+# a, b = map(int, input().split())
+# print(a//b)
+
+# a, b = map(int, input().split())
+# print(a%b)
+
+# f = float(input())
+# print(format(f, ".2f"))
+
+# a, b = map(float, input().split())
+# print(format(a/b,".3f"))
+
+# a, b = map(int, input().split())
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a // b)
+# print(a % b)
+# print(format(a / b,'.2f'))
+
+# a, b, c = map(int, input().split())
+# sum_n = a + b + c
+# avg_n = (a  + b + c)/3
+# print(sum_n, format(avg_n,".2f"))
+
+# n = int(input())
+# print(n<<1)
+
+# a를 2의 b제곱 만큼 곱한 값을 출력, 비트연산자
+a, b = map(int, input().split())
+print(a<<b)
