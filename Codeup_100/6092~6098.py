1f2eaeeb76745319d878154ab6eb8e9604a4f810
lucky.doyun <doyun.jang.0128@gmail.com>
2022-08-22 23:40:52 +0900

    2022.08.22 Python Study

--- Codeup_100/6092~6098.py
+++ Codeup_100/6092~6098.py
@@ -12,13 +12,68 @@
 #     print(d[p], end=' ')
 
 # 6093 이상한 출석 번호 부르기2
-n = int(input())
-s = list(map(int, input().split()))
-p = [0] * n
+# n = int(input())
+# s = list(map(int, input().split()))
+# p = [0] * n
 
-for i in range(0, n):
-    p[i] = s[n - i - 1]
+# for i in range(0, n):
+#     p[i] = s[n - i - 1]
 
-for k in range(0, n):
-    print(p[k], end=' ')
+# for k in range(0, n):
+#     print(p[k], end=' ')
 
+# 6094 이상한 출석 번호 부르기3
+# n = int(input())
+# s = list(map(int, input().split()))
+
+# min = s[0]
+# for i in range(0, n):
+#     if s[i] < min:
+#         min = s[i]
+# print(min)
+
+# 6095 바둑판에 흰 돌 놓기
+# n = int(input())
+
+# arr = []
+
+# for i in range(0, 20):
+#     arr.append([])
+#     for j in range(0, 20):
+#         arr[i].append(0)
+
+# for i in range(0, n):
+#     a, b = map(int, input().split())        
+#     arr[a][b] = 1
+
+# for i in range(1, 20):
+#     for j in range(1, 20):
+#         print(arr[i][j], end=' ')
+#     print()
+
+# # 6096 바둑알 십자 뒤집기(py)
+# arr = []
+# for i in range(0, 19):
+#     arr.append([])
+#     arr[i] = list(map(int, input().split()))
+
+# n = int(input())
+
+# for i in range(0, n):
+#    a, b = map(int, input().split())
+
+#    for k in range(0, 19):
+#     if arr[k][b - 1] == 1:
+#         arr[k][b - 1] = 0
+#     else:
+#         arr[k][b - 1] = 1
+    
+#     if arr[a - 1][k] == 1:
+#         arr[a - 1][k] = 0
+#     else:
+#         arr[a - 1][k] = 1
+
+# for k in range(0, 19):
+#     for j in range(0, 19):
+#         print(arr[k][j], end=' ')
+#     print()
